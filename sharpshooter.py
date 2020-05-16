35126316e9bc0ad8f2720fb7e84186aabf097219
vs-slavchev <vs_slavchev2@abv.bg>
2020-05-16 13:03:41 +0300

    Add logging to file

--- sharpshooter.py
+++ sharpshooter.py
@@ -1,8 +1,13 @@
 import curses
+import logging
+
+logging.basicConfig(filename='last_run.log',level=logging.DEBUG,filemode='w')
 
 def main(standard_screen):
     curses.curs_set(False)
     standard_screen.clear()
+
+    logging.debug("started")
 
     standard_screen.addstr("mandatory curse word to test")
 
