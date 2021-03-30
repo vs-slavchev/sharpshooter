940ddbc49bb91cd7f47cdd289c1c059e3a5eee40
Veselin Slavchev <vs_slavchev@abv.bg>
2021-03-30 19:20:58 +0300

    Filter out non-ordinary files

--- terminal.py
+++ terminal.py
@@ -9,16 +9,22 @@ def get_ls(directory="."):
     all_lines = list(map(lambda s: s.decode("utf-8"), cwd_ls.splitlines()))
     lines = all_lines[2:]  # drop first 2 lines which are not folders
     logging.debug('ls {} output: {} items'.format(directory, len(lines)))
+
+    # drop symbolic links, sockets, named pipes and doors
+    lines = list(filter(lambda l: not l.endswith("@") and\
+          not l.endswith("=") and\
+          not l.endswith("|") and\
+          not l.endswith(">"), lines))
     return lines
 
 
 def open():
     # try different terminals until one of them works
     terminal_commands = [
-            ["exo-open","--working-directory","/Repos","--launch","TerminalEmulator"],
-            ["x-terminal-emulator"],
-            ["urxvt"]
-        ]
+        ["exo-open", "--working-directory", "/Repos", "--launch", "TerminalEmulator"],
+        ["x-terminal-emulator"],
+        ["urxvt"]
+    ]
     for command_array in terminal_commands:
         try:
             subprocess.call(command_array)
