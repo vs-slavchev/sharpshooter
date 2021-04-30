5b179207306a17f90c42bc6f8cc1cc6abd6c377f
Veselin Slavchev <vs_slavchev@abv.bg>
2021-04-30 11:10:33 +0300

    Make setup.py simple

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup
 
 setup(
     name='sharpshooter',
-    py_modules=['sharpshooter'],
+    packages=['sharpshooter'],
     version='0.9.0',
     install_requires=[],
-    entry_points='''
-        [console_scripts]
-        sharpshooter=sharpshooter:main
-    ''',
 )
