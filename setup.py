727809ee4228ddb03f1cbe0abc823de679db802a
vs-slavchev <vs_slavchev@abv.bg>
2022-06-12 20:37:29 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sharpshooter',
-    version='1.3.4',
+    version='1.3.6',
 	description='minimal terminal application that in a single key press lets you navigate and copy/cut/delete/archive your files and open the terminal',
 	author='Veselin Slavchev',
 	author_email='vs_slavchev@abv.bg',
